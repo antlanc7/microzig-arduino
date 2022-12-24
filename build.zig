4eb1d2e959295362dcaccf93b699933487e3655b
Antonio Lanciotti <antonio.lanciotti@live.it>
2022-12-24 17:58:25 +0100

    added avrdude command

--- build.zig
+++ build.zig
@@ -18,4 +18,34 @@ pub fn build(b: *std.build.Builder) !void {
     );
     exe.setBuildMode(.ReleaseSmall);
     exe.install();
+
+    const tty = b.option(
+        []const u8,
+        "tty",
+        "Specify the port to which the Arduino is connected (defaults to COM3)",
+    ) orelse "COM3";
+
+    const bin_path = b.getInstallPath(exe.inner.install_step.?.dest_dir, exe.inner.out_filename);
+
+    const flash_command = blk: {
+        var tmp = std.ArrayList(u8).init(b.allocator);
+        try tmp.appendSlice("-Uflash:w:");
+        try tmp.appendSlice(bin_path);
+        try tmp.appendSlice(":e");
+        break :blk tmp.toOwnedSlice();
+    };
+
+    const upload = b.step("upload", "Upload the code to an Arduino device using avrdude");
+    const avrdude = b.addSystemCommand(&.{
+        "avrdude",
+        "-CC:\\Program Files (x86)\\Arduino\\hardware\\tools\\avr\\etc\\avrdude.conf",
+        "-carduino",
+        "-patmega328p",
+        "-D",
+        "-P",
+        tty,
+        flash_command,
+    });
+    upload.dependOn(&avrdude.step);
+    avrdude.step.dependOn(&exe.inner.install_step.?.step);
 }
